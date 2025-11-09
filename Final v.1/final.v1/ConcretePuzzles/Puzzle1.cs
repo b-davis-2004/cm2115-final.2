b2015ba2718f5dacda5ab95b55baa8b79a7f38b9
b-davis-2004 <b.davis@rgu.ac.uk>
2025-11-09 19:25:57 +0000

    Creating concrete puzzle class for templating

---
+++ Final v.1/final.v1/ConcretePuzzles/Puzzle1.cs
@@ -0,0 +1,9 @@
+namespace final.v1.ConcretePuzzles;
+
+public class Puzzle1
+{
+
+}
+
+//Template for creating specific puzzle types
+//Will be renamed for actual puzzle name later
--- Final v.1/final.v1/obj/Debug/net9.0/final.v1.AssemblyInfo.cs
+++ Final v.1/final.v1/obj/Debug/net9.0/final.v1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("final.v1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df88276b256079932e1f252c405a4e32a9bfb7e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("final.v1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("final.v1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
